672920faa84119031a14dfb6c03a3e48c26bf714
Pranav Vashi <neobuddy89@gmail.com>
2018-08-27 22:36:36 +0530

    softap: sdk: do not advertise warnings as error
    Change-Id: I2da9be8bbc8124f34246bb952a6ccf1f0578480d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- softap/sdk/Android.mk
+++ softap/sdk/Android.mk
@@ -16,6 +16,15 @@ endif
 
 LOCAL_CFLAGS += -DSDK_VERSION=\"0.0.1.0\"
 
+LOCAL_CFLAGS += \
+    -Wno-format \
+    -Wno-enum-conversion \
+    -Wno-sometimes-uninitialized \
+    -Wno-unused-function \
+    -Wno-unused-parameter \
+    -Wno-unused-value \
+    -Wno-unused-variable
+
 LOCAL_USE_VNDK := true
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/qsap_api.h \
